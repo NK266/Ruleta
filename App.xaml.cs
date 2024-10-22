0309de57d0b6e0662251678ef982a90d01fe306b
NK <NK@DESKTOP-KSTQ7V8>
2024-10-22 11:04:16 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ruleta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
